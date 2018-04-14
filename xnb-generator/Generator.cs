1d8a92e96dfce57e39fdb35877e8c3ae0b942d45
Julian Goldsmith <julian@juliangoldsmith.com>
2018-04-13 23:04:09 -0400

    Attempt to clean up marginally

--- xnb-generator/Generator.cs
+++ xnb-generator/Generator.cs
@@ -1,44 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml;
 using System.Xml.Serialization;
-using Mono.Options;
 
 using Schemas;
 
-public class Driver
-{
-	public static int Main(string[] args)
-	{
-		List<string> srcFiles = new List<string>();//opts.RemainingArguments;
-
-		string reference = null;
-		string outName = null;
-        
-		var options = new OptionSet
-		{
-			{ "r|ref=", "Reference", r => reference = r },
-			{ "o|out=", "Output name", o => outName = o },
-		};
-
-		srcFiles = options.Parse(args);
-        
-		LoadTypeMap ("TypeMap");
-		
-		//foreach (string srcRef in opts.References)
-		//	LoadTypeMap (srcRef + ".TypeMap");
-
-		LoadTypeMap (reference + "TypeMap");
-
-		foreach (string src in srcFiles) {
-			Generate (src, outName);
-			SaveTypeMap (outName + "TypeMap");
-		}
-
-		return 0;
-	}
-
+public class Generator
+{   
 	static bool isExtension;
 	static string extName;
 
@@ -51,7 +19,7 @@ public class Driver
 	{
 		StreamReader sr = new StreamReader (fname);
 		XmlSerializer sz = new XmlSerializer (typeof (xcb));
-		xcb xcb = (xcb)sz.Deserialize (sr);
+		xcb xcb = (xcb) sz.Deserialize (sr);
 
 		extName = xcb.extensionxname == null ? "" : xcb.extensionxname;
 		isExtension = extName != "";
@@ -68,7 +36,6 @@ public class Driver
 		cw.WriteLine ("using Xnb.Protocol." + "Xnb" + ";", cwt);
 		cw.WriteLine ("using Xnb.Protocol." + "XProto" + ";", cwt);
 		cw.WriteLine ("", cwt, cwi);
-		//cw.WriteLine ("namespace Xnb", cwt);
 
 		cw.WriteLine ("namespace Xnb");
 		cwt.WriteLine ("namespace Xnb.Protocol." + name);
@@ -127,18 +94,7 @@ public class Driver
 	{
 		if (x.name == null)
 			return;
-
-		/*
-		field f = new field ();
-		f.type = "CARD32";
-		f.name = "Id";
-
-		field[] xItems = new field[1];
-		xItems[0] = f;
-		
-		GenClass (NewTypeToCs (x.name), xItems);
-		*/
-
+        
 		string xName = NewTypeToCs (x.name, "Id");
 
 		cwt.WriteLine ("[StructLayout (LayoutKind.Explicit, Pack=1, CharSet=CharSet.Ansi)]");
@@ -171,12 +127,6 @@ public class Driver
 			cwt.WriteLine ("}");
 			cwt.WriteLine ();
 		}
-		/*
-		cwt.WriteLine ("public static explicit operator " + xName + " (uint xid)");
-		cwt.WriteLine ("{");
-		cwt.WriteLine ("return new " + xName + " (xid);");
-		cwt.WriteLine ("}");
-		*/
 		
 		cwt.WriteLine ("public static explicit operator " + xName + " (Id x)");
 		cwt.WriteLine ("{");
@@ -211,7 +161,6 @@ public class Driver
 			return;
 
 		cwt.WriteLine ("[Error (" + e.number + ")]");
-		//GenClass (NewTypeToCs (ToCs (e.name) + "Error"), e.field, " : " + "Error");
 		GenClass (NewTypeToCs (TypeToCs (e.name) + "Error"), e.field);
 	}
 
@@ -221,7 +170,6 @@ public class Driver
 			return;
 
 		cwt.WriteLine ("[Event (" + e.number + ")]");
-		//GenClass (NewTypeToCs (ToCs (e.name) + "Event"), eItem, " : " + "Event");
 		GenClass (NewTypeToCs (ToCs (e.name) + "Event"), e.Items, " : " + "EventArgs");
 		
 		cw.WriteLine ("public event " + "EventHandler<" + (ToCs (e.name) + "Event") + "> " + (ToCs (e.name) + "Event") + ";");
@@ -233,18 +181,13 @@ public class Driver
 		if (r.name == null)
 			return;
 
-		string inherits;
-		if (isExtension)
-			inherits = "ExtensionRequest";
-		else
-			inherits = "Request";
+		string inherits = isExtension ? "ExtensionRequest" : "Request";
 
 		cwt.WriteLine ("[Request (" + r.opcode + ")]");
 		GenClass (NewTypeToCs (ToCs (r.name) + "Request"), r.Items);
 
 		if (r.reply != null) {
 			cwt.WriteLine ("[Reply (" + r.opcode + ")]");
-			//GenClass (NewTypeToCs (ToCs (r.name) + "Reply"), r.reply.Items, " : " + "Reply");
 			GenClass (NewTypeToCs (ToCs (r.name) + "Reply"), r.reply.Items);
 		}
 	}
@@ -300,34 +243,14 @@ public class Driver
 
 			parms = parms.Trim (',', ' ');
 		}
-
-		/*
-		if (r.reply != null)
-			cw.WriteLine ("[Reply (typeof (" + ToCs (r.name) + "Reply" + "))]");
-		cw.WriteLine ("public void " + ToCs (r.name) + " (" + parms + ")");
-		*/
-		
+  
 		if (r.reply != null)
 			cw.WriteLine ("public " + "Cookie<" + ToCs (r.name) + "Reply" + ">" + " " + ToCs (r.name) + " (" + parms + ")", cwi, ";");
 		else
 			cw.WriteLine ("public " + "void" + " " + ToCs (r.name) + " (" + parms + ")", cwi, ";");
 
 		cw.WriteLine ("{");
-
-		//cw.WriteLine ("ProtocolRequest req = new ProtocolRequest ();");
-		/*
-			 cw.WriteLine ("req.Count = " + 2 + ";");
-			 cw.WriteLine ("req.Extension = " + 0 + ";");
-			 cw.WriteLine ("req.Opcode = " + r.opcode + ";");
-			 cw.WriteLine ("req.IsVoid = " + (r.reply == null ? "1" : "0") + ";");
-			 */
-		/*
-			 cw.WriteLine ("req.Opcode = " + r.opcode + ";");
-			 cw.WriteLine ("req.Data = " + 0 + ";");
-			 cw.WriteLine ("req.Length = " + 4 + ";");
-			cw.WriteLine ();
-			 */
-
+        
 		cw.WriteLine ("" + ToCs (r.name) + "Request req = new " + ToCs (r.name) + "Request ();");
 
 		if (isExtension) {
@@ -337,35 +260,12 @@ public class Driver
 			cw.WriteLine ("req.MessageData.Header.Opcode = " + r.opcode + ";");
 		}
 		cw.WriteLine ();
-
-		/*
-		if (r.Items != null)
-			foreach (object ob in r.Items) {
-				if (ob is field) {
-					field f = ob as field;
-					if (f.name != null) {
-						if (f.name == "roots")
-							Console.Error.WriteLine (f.type);
-						cw.WriteLine ("req.MessageData.@" + ToCs (f.name) + " = @" + ToParm (ToCs (f.name)) + ";");
-					}
-				}
-			}
-			*/
+        
 		foreach (string par in parmList1)
 			cw.WriteLine ("req.MessageData.@" + par + " = @" + ToParm (par) + ";");
 
 		foreach (string par in parmList2)
 			cw.WriteLine ("req.@" + par + " = @" + ToParm (par) + ";");
-
-		/*
-		cw.WriteLine ("unsafe {");
-		//cw.WriteLine (ToCs (r.name) + "RequestData* dp;");
-		cw.WriteLine ("fixed (" + ToCs (r.name) + "RequestData* dp = &req.MessageData) {");
-		cw.WriteLine ("c.Send ((IntPtr)dp, sizeof (" + ToCs (r.name) + "RequestData" + "));");
-		cw.WriteLine ("}");
-		cw.WriteLine ("}");
-		cw.WriteLine ("IntPtr ptr;");
-		*/
 
 		if (r.Items != null)
 			foreach (object ob in r.Items) {
@@ -375,12 +275,7 @@ public class Driver
 						continue;
 					if (l.type != "char")
 						continue;
-
-					/*
-					cw.WriteLine ();
-					cw.WriteLine ("ptr = UnixMarshal.StringToHeap (@" + ToParm (ToCs (l.name)) + ");");
-					cw.WriteLine ("c.Send (ptr, @" + ToParm (ToCs (l.name)) + ".Length);");
-					*/
+     
 					cw.WriteLine ("req.@" + ToCs (l.name) + " = @" + ToParm (ToCs (l.name)) + ";");
 				}
 			}
@@ -397,8 +292,7 @@ public class Driver
 		cw.WriteLine ("}");
 		cw.WriteLine ();
 	}
-
-
+    
 	static void GenEnum (@enum e)
 	{
 		if (e.name == null)
@@ -410,7 +304,6 @@ public class Driver
 
 		foreach (item it in e.item) {
 			cwt.WriteLine (ToCs (it.name) + ",");
-			//cwt.WriteLine (ToCs (it.name) + " = " + it.op.value[0] + " " + it.op.op2 + " " + it.op.value[1] + ",");
 		}
 
 		cwt.WriteLine ("}");
@@ -420,13 +313,6 @@ public class Driver
 	static void GenUnion (@union u)
 	{
 		return;
-
-		//FIXME
-
-		if (u.name != null) {
-			//FIXME: Field offsets as 0
-			GenClass (NewTypeToCs (u.name), u.Items);
-		}
 	}
 
 
@@ -487,8 +373,7 @@ public class Driver
 	static void GenClass (string sName, object[] sItems, string suffix)
 	{
 		Dictionary<string,int> sizeParams = new Dictionary<string,int> ();
-
-		//bool basicStruct = false;
+  
 		bool basicStruct = basic;
 
 		if (sName == "GContext")
@@ -507,19 +392,20 @@ public class Driver
 		bool isEvent = sName.EndsWith ("Event");
 
 		if (!basicStruct) {
-		int structSize = StructSize (sItems);
-		cwt.WriteLine ("[StructLayout (LayoutKind.Explicit, Pack=1, CharSet=CharSet.Ansi, Size=" + structSize + ")]");
-		cwt.WriteLine ("public struct @" + sName + "Data");
-		cwt.WriteLine ("{");
-		if (isRequest) { //TODO: generate one or the other
-			cwt.WriteLine ("[FieldOffset (0)]");
-			cwt.WriteLine ("public Request Header;");
-			cwt.WriteLine ("[FieldOffset (0)]");
-			cwt.WriteLine ("public ExtensionRequest ExtHeader;");
-		}
-		GenClassData (sName + "Data", sItems, "", true);
-		cwt.WriteLine ("}");
-		cwt.WriteLine ();
+    		int structSize = StructSize (sItems);
+    		cwt.WriteLine ("[StructLayout (LayoutKind.Explicit, Pack=1, CharSet=CharSet.Ansi, Size=" + structSize + ")]");
+    		cwt.WriteLine ("public struct @" + sName + "Data");
+    		cwt.WriteLine ("{");
+    		if (isRequest) { //TODO: generate one or the other
+    			cwt.WriteLine ("[FieldOffset (0)]");
+    			cwt.WriteLine ("public Request Header;");
+    			cwt.WriteLine ("[FieldOffset (0)]");
+    			cwt.WriteLine ("public ExtensionRequest ExtHeader;");
+    		}
+
+    		GenClassData (sName + "Data", sItems, "", true);
+    		cwt.WriteLine ("}");
+    		cwt.WriteLine ();
 		}
 
 		if (basicStruct) {
@@ -544,18 +430,6 @@ public class Driver
 		int offset = GenClassData (sName, sItems, "", basicStruct);
 
 		if (!basicStruct) {
-			//cwt.WriteLine ("public byte[] dat;");
-			/*
-			cwt.WriteLine ("public int Read (IntPtr ptr)");
-			cwt.WriteLine ("{");
-			//cwt.WriteLine ("MessageData = (" + sName + "Data" + ")Marshal.PtrToStructure (ptr, typeof(" + sName + "Data" + "));");
-			cwt.WriteLine ("unsafe {");
-			cwt.WriteLine ("MessageData = *(" + sName + "Data" + "*)ptr;");
-			cwt.WriteLine ("}");
-			cwt.WriteLine ("return 0;");
-			cwt.WriteLine ("}");
-			cwt.WriteLine ();
-			*/
 			cwt.WriteLine ("public int Read (IntPtr ptr)");
 			cwt.WriteLine ("{");
 			cwt.WriteLine ("int offset = 0;");
@@ -657,7 +531,6 @@ public class Driver
 				}
 
 				string lType = TypeToCs (l.type);
-				//cwt.WriteLine ("//" + l.type);
 				if (!sizeParams.ContainsKey (l.name)) {
 					Console.Error.WriteLine ("Warning: No length given for " + lName);
 					cwt.WriteLine ("//FIXME: No length given");
@@ -667,9 +540,7 @@ public class Driver
 					else
 						cwt.WriteLine ("[MarshalAs (UnmanagedType.LPArray, SizeParamIndex=" + sizeParams[l.name] + ")]");
 				}
-				////cwt.WriteLine ("//public " + lType + "[]" + " @" + lName + ";");
-				//cwt.WriteLine ("[FieldOffset (" + offset + ")]");
-				////cwt.WriteLine ("//public ValueList<" + lType + ">" + " @" + lName + ";");
+				
 				if (l.type == "CHAR2B" || lType == "sbyte")
 					cwt.WriteLine ("public " + "string" + " @" + lName + ";");
 				else
@@ -678,14 +549,9 @@ public class Driver
 				offset += 4;
 			} else if (ob is valueparam) {
 				valueparam v = ob as valueparam;
-						string vName = "Values";
-
-						if (v.valuelistname != null)
-							vName = ToCs (v.valuelistname);
-
-						string vType = TypeToCs (v.valuemasktype);
-				//cwt.WriteLine ("[FieldOffset (" + offset + ")]");
-				//cwt.WriteLine ("public ValueList<" + TypeToCs (v.valuemasktype) + "> @" + "Values" + ";");
+				string vName = (v.valuelistname == null) ? "Values" : ToCs(v.valuelistname);               
+				string vType = TypeToCs (v.valuemasktype);
+				
 				cwt.WriteLine ("//public ValueList<" + vType + "> @" + vName + ";");
 				cwt.WriteLine ("public " + vType + "[] @" + vName + ";");
 				offset += 4;
@@ -768,19 +634,10 @@ public class Driver
 								offset += SizeOfType (fType);
 							}
 						}
-
-						/*
-						//TODO: use fieldref
-						if (f.name.EndsWith ("_len")) {
-							string fNameArray = f.name.Substring (0, f.name.Length - 4);
-							sizeParams[fNameArray] = index;
-							cwt.WriteLine ("//TODO: private");
-							cwt.WriteLine ("//public " + fType + " @" + fName + ";");
-						} else {
-						*/
-							if (withOffsets) {
+      
+						if (withOffsets) {
 							cwt.WriteLine ("public " + fType + " @" + fName + ";");
-							} else {
+						} else {
 							cwt.WriteLine ("public " + fType + " @" + fName);
 							cwt.WriteLine ("{");
 							cwt.WriteLine ("get {");
@@ -789,8 +646,7 @@ public class Driver
 							cwt.WriteLine ("MessageData.@" + fName + " = value;");
 							cwt.WriteLine ("}");
 							cwt.WriteLine ("}");
-							}
-						//}
+						}
 						index++;
 					}
 				} else if (ob is pad) {
@@ -904,7 +760,6 @@ public class Driver
 			return typeMap[name];
 
 		Console.Error.WriteLine ("Warning: typeMap doesn't contain " + name);
-		//return "IntPtr";
 		return ToCs (name);
 	}
 
@@ -915,7 +770,6 @@ public class Driver
 
 	static string NewTypeToCs (string name, string suffix)
 	{
-		//TODO: don't look in typeMap twice?
 		//TODO: error reporting?
 
 		string cs;
@@ -941,7 +795,7 @@ public class Driver
 	static Dictionary<string,string> typeMap = new Dictionary<string,string> ();
 	//TODO: use caseMap for field case corrections?
 	static Dictionary<string,string> caseMap = new Dictionary<string,string> ();
-	static void LoadTypeMap (string fname)
+	public static void LoadTypeMap (string fname)
 	{
 		char[] delim = {'\t'};
 
@@ -972,26 +826,15 @@ public class Driver
 		sr.Close ();
 	}
 
-	static void SaveTypeMap (string fname)
+	public static void SaveTypeMap (string fname)
 	{
 		StreamWriter sw = new StreamWriter (new FileStream (fname, FileMode.Create, FileAccess.Write));
 		sw.WriteLine ("#TypeMap for " + "[]");
 		sw.WriteLine ("#Generated by xnb-generator");
 		sw.WriteLine ();
-		
-		/*
-		foreach (KeyValuePair<string,string> entry in sizeMap)
-			sw.WriteLine (entry.Key + "\t" + entry.Value);
-		sw.WriteLine ();
-		*/
 
 		foreach (KeyValuePair<string,string> entry in typeMap)
 			sw.WriteLine (entry.Key + "\t" + entry.Value);
-
-		/*
-		foreach (KeyValuePair<string,string> entry in idMap)
-			sw.WriteLine (entry.Key + "\t" + entry.Value);
-		*/
 
 		sw.Close ();
 	}
---
+++ xnb-generator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Options;
+
+namespace xnbgenerator
+{
+    public class Program
+	{
+        public static int Main(string[] args)
+        {
+            string reference = null;
+            string outName = null;
+
+            var options = new OptionSet
+            {
+                { "r|ref=", "Reference", r => reference = r },
+                { "o|out=", "Output name", o => outName = o },
+            };
+
+            List<string> srcFiles = options.Parse(args);
+
+            if (string.IsNullOrEmpty(reference))
+            {
+                Console.WriteLine("Must have reference");
+                return 1;
+            }
+
+            if (string.IsNullOrEmpty(outName))
+            {
+                Console.WriteLine("Must have output name");
+                return 1;
+            }
+
+            Generator.LoadTypeMap("TypeMap");
+			Generator.LoadTypeMap(reference + "TypeMap");
+
+            foreach (string src in srcFiles)
+            {
+				Generator.Generate(src, outName);
+				Generator.SaveTypeMap(outName + "TypeMap");
+            }
+
+            return 0;
+        }
+    }
+}
